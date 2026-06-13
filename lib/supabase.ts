1f8abfc99ddb463730d9a6a231dadefc808dcbf5
manideepgaddameedi <manideepgaddameedi1@gmail.com>
2026-06-13 09:18:26 +0530

    Update Supabase client initialization with new keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://ltzfloharlymlreksszw.supabase.co",
+
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0emZsb2hhcmx5bWxyZWtzc3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMzIwMjQsImV4cCI6MjA5NjgwODAyNH0.nh-7IuqbinosrrHKbmsDgLiSpg8ccjGQgYrsM-DLxC4"
 );
